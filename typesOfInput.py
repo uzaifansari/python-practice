6c3cc5a8e048355c29a09ad8bcb512f601cc6470
Uzaif Ansari <114265175+uzaifansari@users.noreply.github.com>
2026-03-21 04:43:08 +0530

    Create typesOfInput.py

---
+++ typesOfInput.py
@@ -0,0 +1,16 @@
+# space separated array VERY IMPORTANT
+# arr = list(map(int, input().split())) # 4 5 0 1 9 0 5 0
+# print(arr) # will return in array format.
+
+# comma separated array VERY IMPORTANT
+# arr = list(map(int, input().split(','))) # 4,5,0,1,9,0,5,0
+# print(arr) # will return in array format.
+
+# comma separated array WITH BRACKETS VERY IMPORTANT
+# s = input().strip("[]")
+# arr = list(map(int, s.split(','))) # [4,5,0,1,9,0,5,0]
+# print(arr) # will return in array format.
+
+#  space separated string of words to array
+words = input().split() #apple banana cherry mango
+print(words)
