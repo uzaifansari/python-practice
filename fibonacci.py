3fee4f5145597e8d65cc065efef742e53800af35
Uzaif Ansari <114265175+uzaifansari@users.noreply.github.com>
2026-03-21 04:44:05 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,25 @@
+# FIBBONACCI PRINT NUMBERS LESS THAN N 
+# n = 10
+# a,b = 0,1
+# if n <= 0:
+#     print(False)
+# elif n==1:
+#     print(a)
+# else:
+#     while a<n:
+#         print(a, end=" ")
+#         a,b=b,a+b
+
+# FIBBONACCI PRINT FIRST N 
+# n = 10
+# a,b =0,1
+# count = 0
+# if n <= 0:
+#     print(False)
+# elif n==1:
+#     print(a)
+# else:
+#     while count<n:
+#         print(a, end=" ")
+#         a,b=b,a+b
+#         count+=1
